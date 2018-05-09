370ed3c880ff4b63b98120f49cea80c0c0e3f27e
Aldren Terante <aldrenterante@altus-dc.com>
2018-05-09 19:18:13 +0800

    add sanitize.css and add google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,11 +6,20 @@ module.exports = {
     {
       resolve: 'gatsby-source-contentful',
       options: {
-        spaceId: 'j4bmp3cn7p2r',
-        accessToken: '505a4954c197b3b6bb9d7063da859eea5d25eaa1bf65d133c090a89671a89cc5',
+        spaceId: process.env.CF_SPACE || 'j4bmp3cn7p2r',
+        accessToken: process.env.CF_TOKEN || '505a4954c197b3b6bb9d7063da859eea5d25eaa1bf65d133c090a89671a89cc5'
       }
     },
-    `gatsby-transformer-remark`,
-    `gatsby-plugin-react-helmet`
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Libre Baskerville\:100`,
+          `source sans pro\:300,400` // you can also specify font weights and styles
+        ]
+      }
+    },
+    'gatsby-transformer-remark',
+    'gatsby-plugin-react-helmet'
   ],
 }
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
+import 'sanitize.css/sanitize.css';
+
 import './index.css'
 
 const Header = () => (
